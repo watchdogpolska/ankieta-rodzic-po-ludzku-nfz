27b44a74757a8472467b0f051cfe1ee782beb98a
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-11-30 10:45:01 +0100

    Add Survey.Survey.{city,voivodeship} field

--- survey/models.py
+++ survey/models.py
@@ -80,6 +80,8 @@ class Hospital(TimeStampedModel):
         help_text=_('The internal identifier used to connect foundation database'),
         max_length=15,
         blank=True)
+    voivodeship = models.CharField(max_length=100, verbose_name=_("Voivodeship"))
+    city = models.CharField(max_length=100, verbose_name=_("City"))
     objects = HospitalQuerySet.as_manager()
 
     class Meta:
